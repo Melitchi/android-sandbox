c9f4f6bc2001702404c2c081de40bd7a65551e91
Melitchi <melissa.fontaine4@gmail.com>
2016-11-07 11:52:21 +0100

    init step 2

---
+++ Step2/app/src/main/java/com/android/melitchi/atelier0/TheIncredibleQuestion.java
@@ -0,0 +1,12 @@
+package com.android.melitchi.atelier0;
+
+/**
+ * Created by fonta on 07/11/2016.
+ */
+
+public class TheIncredibleQuestion {
+    public static String function (){
+
+        return "42";
+    }
+}
